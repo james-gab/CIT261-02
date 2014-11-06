4ba3d0a9ce83d43b09cd83df13e6a20a17f0b1d5
gab James <jam12012@byui.edu>
2014-11-05 22:02:06 -0600

    gab - added Lesson 07

---
+++ kan/sign_in.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="../css/login_css.css" rel="stylesheet" type="text/css" media="screen" />
+    </head>
+    <body>
+        <div id="main">
+            <h1>We Serve</h1>
+            <div id="login">
+                <h2>Login</h2>
+                <form action="" method="post">
+                    <label>UserName :</label>
+                    <input id="name" name="username" placeholder="username" type="text">
+                    <label>Password :</label>
+                    <input id="password" name="password" placeholder="**********" type="password">
+                    <span style="display: inline;">
+                        <input name="submit" type="submit" value=" Login "> <input name="register" type="submit" value=" Register "/>
+                    </span>                   
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
