c70a5fcc33bdebfb6b98041ff59e5345d251ead2
gab James <jam12012@byui.edu>
2014-12-16 18:52:14 -0600

    gab - Lesson 12 - fixed several small issues and corrected dead links.

--- c/contact_main.php
+++ c/contact_main.php
@@ -2,116 +2,149 @@
 <html lang="en">
 
     <head>
-        <?php include_once $_SERVER['DOCUMENT_ROOT'].'/css/meta.php';?>
+        <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/css/meta.php'; ?>
         <title>Contact&#32;page&#32;&#124;&#32;iServe&#46;com</title>
-        
-<!--[if lt IE 9]>
-        <script src="js/dist/html5shiv.js" type="text/javascript"></script>
-    <![endif]-->
+
+        <!--[if lt IE 9]>
+                <script src="js/dist/html5shiv.js" type="text/javascript"></script>
+            <![endif]-->
 
 
 
-         
-<!-- calls CSS file -->
-<link href="../css/home_page.css" rel="stylesheet" type="text/css" media="screen" />
-<!-- calls Google google_analyticstracking.js -->
-<!--<script src="../../js/google_analyticstracking.js" type="text/javascript"></script>
--->
+
+        <!-- calls CSS file -->
+        <link href="../css/home_page.css" rel="stylesheet" type="text/css" media="screen" />
+        <!-- calls Google google_analyticstracking.js -->
+        <!--<script src="../../js/google_analyticstracking.js" type="text/javascript"></script>
+        -->
+
+        <script>
+//check if "user" is in localStorage
+
+            function setItemForm1b()
+            {
+                var form1b = document.getElementById('form1b');
+                var SOneDateFrom = document.getElementById('form1b.SOneDateFrom');
+                localStorage.setItem("SOneDateFrom", JSON.stringify(form1b.SOneDateFrom.value));
+            }
+
+            function getItemForm1b() {
+//    var SOneDateFrom = JSON.parse(localStorage.getItem('SOneDateFrom'));
+                var SOneDateFrom = localStorage.getItem('SOneDateFrom');
+                alert('getItemForm1B: ' + SOneDateFrom);
+                form1b.SOneDateFrom.value = SOneDateFrom; // shows quotes!
+            }
+
+            function deleteStorage() {
+                form1b.SOneDateFrom.value = localStorage.setItem('SOneDateFrom', '');
+                if (form1b.SOneDateFrom.value === "undefined") {
+                    form1b.SOneDateFrom.value = "";
+                }
+                alert('deleteItemForm1B: ' + SOneDateFrom);
+            }
+
+        </script>
+
+
     </head>
-    
-    
+
+
     <body>
         <header>
-        
-<!--Logo-->
+
+            <!--Logo-->
 
             <div> 
-                <?php include $_SERVER['DOCUMENT_ROOT'].'/nav/logo_header.php'; ?> 
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/nav/logo_header.php'; ?> 
             </div>
-       
-<!-- header navigation area -->
-<!--Calls from file headernavigation.php -->
-        <div> 
-            <?php include $_SERVER['DOCUMENT_ROOT'].'/nav/headernavigation.php'; ?> 
-        </div>
+
+            <!-- header navigation area -->
+            <!--Calls from file headernavigation.php -->
+            <div> 
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/nav/headernavigation.php'; ?> 
+            </div>
         </header>
 
 
 
-<!-- Main body -->
-    <main>
-        
-        <h2>Contact Page</h2>
-        <br>
-       
-        
-       <?php
+        <!-- Main body -->
+        <main>
 
-    if(!empty($reply)){
-
-      echo "<p class='notify'>$reply</p>";
-
-    }
-
-    unset($reply);
-
-?> 
-        
-        
-        
-        <form class="contact" method="post" action="index.php" id="contactform">
-
-<fieldset>
-    <p>All fields are required.</p> 
-        
-    <table>
-        <tr>
-            <td class="r"><label for="firstname">Name&#58; </label></td>
-            <td><input type="text" name="firstname" id="firstname" value=" <?php echo $firstname; ?>" ></td>
-        </tr>
-        <tr>
-            <td class="r"><label for="email">Email Address&#58; </label></td>
-            <td><input type="email" name="email" id="email" value="<?php echo $email; ?>" ></td>
-        </tr>
-        <tr>
-            <td class="r"><label for="subject">Subject&#58; </label></td>
-            <td><input type="text" name="subject" id="subject" value="<?php echo $subject; ?>" ></td>
-        </tr>
-    </table>
-
-<label for="message">Message</label><br><textarea class="contact" name="message" id="message" ><?php echo $message; ?></textarea><br>
-
-<br>
-<p class="f">Security Code</p>
-<img src="antispam.php" width="140" height="50" alt="nospam"><br>
- 
-<p class="e">(Type these characters into the text box below)</p>
+            <h2>Contact Page</h2>
+            <br>
 
 
-<table>
-    <tr>
-        <td><input name="anti_spam_code" id="anti_spam_code"></td>
-        <td><input type="submit" name="action" id="action" value="Send"></td>
-    </tr>
-</table>
+            <?php
+            if (!empty($reply)) {
+
+                echo "<p class='notify'>$reply</p>";
+            }
+
+            unset($reply);
+            ?> 
 
 
 
-</fieldset>
-      
-</form> 
-         
-       
-    </main>
+            <form class="contact" method="post" action="index.php" id="contactform">
 
-    
-    <footer>
-<!--Calls Footer from file footer.index.php -->
-        <div> 
-            <?php include $_SERVER['DOCUMENT_ROOT'].'/nav/footer.php'; ?> 
-        </div>
+                <fieldset>
+                    <p>All fields are required.</p> 
 
-    </footer>
-   
+                    <table>
+                        <tr>
+                            <td class="r"><label for="firstname">Name&#58; </label></td>
+                            <td><input type="text" name="firstname" id="firstname" value=" <?php echo $firstname; ?>" ></td>
+                        </tr>
+                        <tr>
+                            <td class="r"><label for="email">Email Address&#58; </label></td>
+                            <td><input type="email" name="email" id="email" value="<?php echo $email; ?>" ></td>
+                        </tr>
+                        <tr>
+                            <td class="r"><label for="subject">Subject&#58; </label></td>
+                            <td><input type="text" name="subject" id="subject" value="<?php echo $subject; ?>" ></td>
+                        </tr>
+                    </table>
+
+                    <label for="message">Message</label><br><textarea class="contact" name="message" id="message" ><?php echo $message; ?></textarea><br>
+
+                    <br>
+                    <p class="f">Security Code</p>
+                    <img src="antispam.php" width="140" height="50" alt="nospam"><br>
+
+                    <p class="e">(Type these characters into the text box below)</p>
+
+
+                    <table>
+                        <tr>
+                            <td><input name="anti_spam_code" id="anti_spam_code"></td>
+                            <td><input type="submit" name="action" id="action" value="Send"></td>
+                        </tr>
+                    </table>
+
+
+
+                </fieldset>
+
+            </form> 
+
+            <textarea id="user" style="margin-bottom: 4px; width: 250px; height: 20px">
+            </textarea><br />
+            <button id="save">Save</button>
+            <div id="result"></div>
+
+            <!--<button id="save">Clear</button>-->
+
+
+        </main>
+
+
+        <footer>
+            <!--Calls Footer from file footer.index.php -->
+            <div> 
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/nav/footer.php'; ?> 
+            </div>
+
+        </footer>
+
     </body>
 </html>
